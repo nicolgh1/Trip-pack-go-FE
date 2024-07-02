6a5a2cd15ff580474cbe482a4dd1a213a30af1cf
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-07-02 12:14:09 +0100

    Creates temporary example of firebase implementation work

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { db } from './firebaseConfig';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const colRef = collection(db, "test-collection");
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(colRef, (snapshot) => {
+      const dbData = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log('useEffect');
+      setData(dbData);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <View style={styles.container}>
+      <View><Text>{data[0]?.testColumn || "Loading..."}</Text></View>
       <Text>Trip Pack Go!</Text>
       <StatusBar style="auto" />
     </View>
