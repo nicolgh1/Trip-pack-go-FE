3d6517e8ff706811d0143164e723c809781631f3
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-07-03 14:16:16 +0100

    Ensures app is only accessible after login

--- App.js
+++ App.js
@@ -1,18 +1,58 @@
 //import { StatusBar } from 'expo-status-bar';
-import { collection, onSnapshot } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { db } from './firebaseConfig';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { db, firebaseAuth } from "./firebaseConfig";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomePage from './src/Pages/2.HomePage';
-import SearchPage from './src/Pages/3.SearchPage'
-import UserAccountSettingsPage from './src/Pages/5.UserAccountSettingsPage';
-import UserItinerariesPage from './src/Pages/6.UserItinerariesPage';
-import PackingOptionsPage from './src/Pages/7.PackingOptionsPage';
+import HomePage from "./src/Pages/2.HomePage";
+import SearchPage from "./src/Pages/3.SearchPage";
+import UserAccountSettingsPage from "./src/Pages/5.UserAccountSettingsPage";
+import UserItinerariesPage from "./src/Pages/6.UserItinerariesPage";
+import PackingOptionsPage from "./src/Pages/7.PackingOptionsPage";
+import StartPage from "./src/Pages/1.StartPage";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Stack = createNativeStackNavigator();
+
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen
+        name="HomePage"
+        component={HomePage}
+        options={{ headerShown: false }}
+      ></InsideStack.Screen>
+
+      <InsideStack.Screen
+        name="SearchPage"
+        component={SearchPage}
+        options={{ headerShown: false }}
+      ></InsideStack.Screen>
+
+      <InsideStack.Screen
+        name="UserAccountSettingsPage"
+        component={UserAccountSettingsPage}
+        options={{ headerShown: false }}
+      ></InsideStack.Screen>
+
+      <InsideStack.Screen
+        name="UserItinerariesPage"
+        component={UserItinerariesPage}
+        options={{ headerShown: false }}
+      ></InsideStack.Screen>
+
+      <InsideStack.Screen
+        name="PackingOptionsPage"
+        component={PackingOptionsPage}
+        options={{ headerShown: false }}
+      ></InsideStack.Screen>
+    </InsideStack.Navigator>
+  );
+}
 
 export default function App() {
   // const [data, setData] = useState([]);
@@ -29,26 +69,31 @@ export default function App() {
   //   });
   //   return () => unsubscribe();
   // }, []);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      console.log("user", user);
+      setUser(user);
+    });
+  }, []);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-
-      <Stack.Screen name="HomePage" component={HomePage} options={{ headerShown: false }}>  
-        </Stack.Screen>
-
-        <Stack.Screen name="SearchPage" component={SearchPage} options={{ headerShown: false }}>
-        </Stack.Screen>
-
-        <Stack.Screen name="UserAccountSettingsPage" component={UserAccountSettingsPage} options={{ headerShown: false }}>
-        </Stack.Screen>
-
-        <Stack.Screen name="UserItinerariesPage" component={UserItinerariesPage} options={{ headerShown: false }}>
-        </Stack.Screen>
-
-        <Stack.Screen name="PackingOptionsPage" component={PackingOptionsPage} options={{ headerShown: false }}>
-        </Stack.Screen>
-
+      <Stack.Navigator initialRouteName="StartPage">
+        {user ? (
+          <Stack.Screen
+            name="Inside"
+            component={InsideLayout}
+            options={{ headerShown: false }}
+          ></Stack.Screen>
+        ) : (
+          <Stack.Screen
+            name="StartPage"
+            component={StartPage}
+            options={{ headerShown: false }}
+          ></Stack.Screen>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -57,8 +102,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
