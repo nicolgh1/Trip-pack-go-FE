4020392045a9f3af816a77270ac07f0984270499
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-07-03 14:15:33 +0100

    Creates logout functionality

--- src/Pages/5.UserAccountSettingsPage.jsx
+++ src/Pages/5.UserAccountSettingsPage.jsx
@@ -1,34 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Button,
+} from "react-native";
 
-import Header from '../components/Header';
-import Footer from '../components/FooterNavigation';
+import Header from "../components/Header";
+import Footer from "../components/FooterNavigation";
+import { firebaseAuth } from "../../firebaseConfig";
 
 export default function UserAccountSettingsPage({ navigation }) {
-    
-
-    return (
-        <View style={styles.screen}>
-            <Header />
-            <View style={styles.body}>
-                <Text>Hello from user account</Text>
-            </View>
-            <Footer navigation={navigation} />
-        </View>
-    )
+  return (
+    <View style={styles.screen}>
+      <Header />
+      <View style={styles.body}>
+        <Text>Hello from user account</Text>
+        <Button onPress={() => firebaseAuth.signOut()} title="LOGOUT"/>
+      </View>
+      <Footer navigation={navigation} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        backgroundColor: '#fff',
-        justifyContent: 'space-between'
-    },
-    body: {
-        flex: 8,
-        width: '100%',
-        backgroundColor: '#14141410',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+  },
+  body: {
+    flex: 8,
+    width: "100%",
+    backgroundColor: "#14141410",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
