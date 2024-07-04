2802e2adc63df23b0ded62d466a69e39c389c19f
stefan <lioncatnewyork@gmail.com>
2024-07-04 20:43:26 +0100

    ui adjustments

--- src/Pages/6.1.UserItineraryDetailPage.jsx
+++ src/Pages/6.1.UserItineraryDetailPage.jsx
@@ -1,75 +1,90 @@
 import { act } from "react";
-import { View, Text, TouchableOpacity, ScrollView, } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import Footer from "../components/FooterNavigation";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
+export default function UserItineraryDetailPage({
+  itineraryId,
+  userItineraries,
+  setCurrentItinerary,
+  navigation,
+}) {
 
-export default function UserItineraryDetailPage({ itineraryId, userItineraries, setCurrentItinerary}) {
-    const currentItinerary = userItineraries.find((itinerary) => itinerary.id === itineraryId);
+  
+  const currentItinerary = userItineraries.find(
+    (itinerary) => itinerary.id === itineraryId
+  );
 
-    return (
-        <View>
-            <Text style={styles.header}>Itinerary Detail Page</Text>
-            <TouchableOpacity onPress={() => setCurrentItinerary(null)}>
-                <Text style={styles.backToItineraries}>Back to itineraries</Text>
-            </TouchableOpacity>
-            <ScrollView>
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <TouchableOpacity onPress={() => setCurrentItinerary(null)}>
+        <Text style={styles.header}> {currentItinerary.destination}</Text>
+        <Text style={styles.backToItineraries}>Back to itineraries</Text>
+      </TouchableOpacity>
 
-            <Text style={styles.header}>Destination: {currentItinerary.destination}</Text>
-            {currentItinerary.itineraryDetails.map((itineraryDetail) => {
-                return (
-                    <View key={itineraryDetail.day} style={styles.userItinerariesStyle}>
-                        <Text style={styles.day}>Day: {itineraryDetail.day}</Text>
-                        <Text style={styles.activity}>Activity: {itineraryDetail.activity}</Text>
-                    </View>
-                )
-            })}
+      <ScrollView>
+        {currentItinerary.itineraryDetails.map((itineraryDetail) => {
+          return (
+            <View key={itineraryDetail.day} style={styles.userItinerariesStyle}>
+              <Text style={styles.day}>Day: {itineraryDetail.day}</Text>
+              <Text style={styles.activity}>
+                Activity: {itineraryDetail.activity}
+              </Text>
 
-            </ScrollView>
+              <TouchableOpacity>
+                <Text style={styles.editButton}>Edit</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </ScrollView>
 
-        </View>
-    )
-
+      {/* <Footer navigation={navigation} /> */}
+    </SafeAreaView>
+  );
 }
 
 const styles = {
-    header: {
-        fontSize: 24,
-        fontWeight: "bold",
-        textAlign: "center",
-        margin: 20,
-    },
-    backToItineraries: {
-        fontSize: 18,
-        textAlign: "center",
-        margin: 10,
-    },
-    day: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    activity: {
-        fontSize: 16,
-    },
-    userItinerariesStyle: {
-        flex: 1,
-        backgroundColor: "#1caccf",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: 10,
-        padding: 10,
-        margin: 10,
-        width: 300,
-        height: 250,
-        borderColor: "black",
-        borderWidth: 3,
-      },
-      header: {
-        backgroundColor: "#1caccf",
-        color: "white",
-        padding: 10,
-        margin: 10,
-        width: 300,
-        border: "1px solid black",
-        fontSize: 20,
-      },
-}
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    top: 0,
+  },
+  backToItineraries: {
+    fontSize: 18,
+    textAlign: "center",
+    margin: 10,
+    backgroundColor: "black",
+    color: "white",
+    borderRadius: 20,
+  },
+  day: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  activity: {
+    fontSize: 20,
+  },
+  userItinerariesStyle: {
+    margin: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 20,
+    backgroundColor: "#ffffff",
+    position: "relative",
+    textAlign: "center",
+    textAlignVertical: "center",
+    height: 220,
+  },
+  editButton: {
+    backgroundColor: "black",
+    color: "white",
+    textAlign: "center",
+    borderRadius: 20,
+    top: 141,
+    width: 130,
+    height: 23,
+  },
+};
