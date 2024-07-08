3e8563e7259cc9596ea7bdb4d1e61041956dc482
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-07-08 12:22:26 +0100

    Shows user information

--- src/Pages/5.UserAccountSettingsPage.jsx
+++ src/Pages/5.UserAccountSettingsPage.jsx
@@ -1,22 +1,90 @@
-import { StyleSheet, View, Text, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+  FlatList,
+  Modal,
+} from "react-native";
 import Header from "../components/Header";
 import Footer from "../components/FooterNavigation";
 import { firebaseAuth } from "../../firebaseConfig";
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
+import UserInfoEditForm from "../components/UserInfoEditForm";
 
 export default function UserAccountSettingsPage({ navigation }) {
   const { user, loading } = useContext(UserContext);
+  const [packingList, setPackingList] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const addItem = () => {
+    if (newItem.trim().length > 0) {
+      setPackingList([...packingList, newItem]);
+      setNewItem("");
+    }
+  };
+
+  const deleteItem = (index) => {
+    const newList = [...packingList];
+    newList.splice(index, 1);
+    setPackingList(newList);
+  };
 
   if (loading) return <Text>Loading...</Text>;
+
   return (
     <View style={styles.screen}>
       <Header />
       <View style={styles.body}>
-        {/* <Text>{user.email? user.email : null}</Text> */}
-        <Text>Hello from user account</Text>
+        <View style={styles.userInfo}>
+          <View style={styles.editUserButton}>
+            <Button title="Edit" onPress={() => setIsModalVisible(true)} />
+          </View>
+          <Text style={styles.label}>User Name: {user.username}</Text>
+          <Text style={styles.label}>First Name: {user.firstName}</Text>
+          <Text style={styles.label}>Surname: {user.surname}</Text>
+          <Text style={styles.label}>Sex: {user.sex}</Text>
+          <Text style={styles.label}>
+            Home Location: {user.countryOfResidence}
+          </Text>
+          <Text style={styles.label}>Email Address: {user.emailAddress}</Text>
+        </View>
+        <View style={styles.packingList}>
+          <Text style={styles.sectionTitle}>Packing List Musts</Text>
+          <View style={styles.addItem}>
+            <TextInput
+              style={styles.input}
+              placeholder="Add Item"
+              value={newItem}
+              onChangeText={setNewItem}
+            />
+            <View style={styles.buttonAdd}>
+              <Button title="+" onPress={addItem} />
+            </View>
+          </View>
+          <FlatList
+            data={packingList}
+            renderItem={({ item, index }) => (
+              <View style={styles.listItem}>
+                <Text>{item}</Text>
+                <Button title="Delete" onPress={() => deleteItem(index)} />
+              </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
         <Button onPress={() => firebaseAuth.signOut()} title="LOGOUT" />
       </View>
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        animationType="slide"
+      >
+        <UserInfoEditForm setIsModalVisible={setIsModalVisible} />
+      </Modal>
       <Footer navigation={navigation} />
     </View>
   );
@@ -33,6 +101,68 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "#14141410",
     alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
+  },
+  userInfo: {
+    width: "100%",
+    marginTop: 30,
+    marginBottom: 30,
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+  },
+  editUserButton: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  packingList: {
+    width: "100%",
+    marginBottom: 20,
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+  },
+  addItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderBottomColor: "#ddd",
+    borderBottomWidth: 1,
+  },
+  buttonAdd: {
+    width: 60,
   },
 });
