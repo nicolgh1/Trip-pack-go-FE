37ac611dc7b12dcccb7715bbebad2e4e02b57cfa
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-07-08 15:03:29 +0100

    Adds user feedback err handling and go back button

--- src/components/UserInfoEditForm.jsx
+++ src/components/UserInfoEditForm.jsx
@@ -9,8 +9,11 @@ export default function UserInfoEditForm({ setIsModalVisible }) {
   const { user, loading } = useContext(UserContext);
 
   const [username, setUsername] = useState(user.username || "");
+  const [usernameErrMsg, setUsernameErrorMsg] = useState("");
   const [firstName, setFirstName] = useState(user.firstName || "");
+  const [firstNameErrMsg, setFirstNameErrorMsg] = useState("");
   const [surname, setSurname] = useState(user.surname || "");
+  const [surnameErrMsg, setSurnameErrorMsg] = useState("");
   const [sex, setSex] = useState(user.sex || "");
   const [countryOfResidence, setCountryOfResidence] = useState(
     user.countryOfResidence || ""
@@ -79,16 +82,39 @@ export default function UserInfoEditForm({ setIsModalVisible }) {
   ];
 
   const handleSave = () => {
-    const updatedUserObj = {
-      username,
-      firstName,
-      surname,
-      sex,
-      countryOfResidence,
-    };
-    const userDocRef = doc(db, "users", user.id);
-    updateDoc(userDocRef, updatedUserObj);
-    setIsModalVisible(false);
+    let isValid = true;
+
+    if (!username) {
+      setUsernameErrorMsg("Please Add a Username");
+      isValid = false;
+    } else {
+      setUsernameErrorMsg("");
+    }
+    if (!firstName) {
+      setFirstNameErrorMsg("Please Add a First Name");
+      isValid = false;
+    } else {
+      setFirstNameErrorMsg("");
+    }
+    if (!surname) {
+      setSurnameErrorMsg("Please Add a Surname");
+      isValid = false;
+    } else {
+      setSurnameErrorMsg("");
+    }
+
+    if (isValid) {
+      const updatedUserObj = {
+        username,
+        firstName,
+        surname,
+        sex,
+        countryOfResidence,
+      };
+      const userDocRef = doc(db, "users", user.id);
+      updateDoc(userDocRef, updatedUserObj);
+      setIsModalVisible(false);
+    }
   };
 
   return (
@@ -100,6 +126,9 @@ export default function UserInfoEditForm({ setIsModalVisible }) {
         onChangeText={setUsername}
         placeholder="Username"
       />
+      {usernameErrMsg ? (
+        <Text style={styles.errMsg}>{usernameErrMsg}</Text>
+      ) : null}
       <Text>First Name</Text>
       <TextInput
         style={styles.input}
@@ -107,6 +136,9 @@ export default function UserInfoEditForm({ setIsModalVisible }) {
         onChangeText={setFirstName}
         placeholder="First Name"
       />
+      {firstNameErrMsg ? (
+        <Text style={styles.errMsg}>{firstNameErrMsg}</Text>
+      ) : null}
       <Text>Surname</Text>
       <TextInput
         style={styles.input}
@@ -114,6 +146,9 @@ export default function UserInfoEditForm({ setIsModalVisible }) {
         onChangeText={setSurname}
         placeholder="Surname"
       />
+      {surnameErrMsg ? (
+        <Text style={styles.errMsg}>{surnameErrMsg}</Text>
+      ) : null}
       <Text>Sex</Text>
       <Picker
         selectedValue={sex}
@@ -136,6 +171,7 @@ export default function UserInfoEditForm({ setIsModalVisible }) {
         })}
       </Picker>
       <Button title="Save" onPress={handleSave} />
+      <Button title="GO BACK" onPress={() => setIsModalVisible(false)}></Button>
     </>
   );
 }
@@ -156,5 +192,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     margin: 5,
+  },
+  errMsg: {
+    color: "red",
   },
 });
