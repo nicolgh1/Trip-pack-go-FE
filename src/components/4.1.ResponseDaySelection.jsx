d9e089a02d94a474667cbdc77e01d3ca75a6982c
nicolgh <nicol.herta1@gmail.com>
2024-07-10 20:19:51 +0100

    completes styling for response page

--- src/components/4.1.ResponseDaySelection.jsx
+++ src/components/4.1.ResponseDaySelection.jsx
@@ -1,132 +1,218 @@
-import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Image} from "react-native"
-import {fetchPlacePicture} from '../../googleApi'
-import { useState } from "react"
-import { MoreActivitiesSelection } from "./4.2.MoreActivitiesSelection"
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { useState } from "react";
 
-export const ResponseDaySelection = ({ attractions, currentDay, setCurrentDay, responseObj, setResponseObj }) => {
+export const ResponseDaySelection = ({
+  attractions,
+  currentDay,
+  setCurrentDay,
+  responseObj,
+  setResponseObj,
+  route, 
+  navigation
+}) => {
+  const [mainActivity, setMainActivity] = useState({});
 
-    const [mainActivity, setMainActivity] = useState({})
-  
-    const handleNextDay = () => {
-      if (currentDay < responseObj.total_days) {
-        setCurrentDay(currentDay + 1)
-        setMainActivity({})
-      } else if(currentDay === responseObj.total_days) {
-        setCurrentDay(responseObj.total_days+1)
-      }
+  const handleNextDay = () => {
+    if (currentDay < responseObj.total_days) {
+      setCurrentDay(currentDay + 1);
+      setMainActivity({});
+    } else if (currentDay === responseObj.total_days) {
+      setCurrentDay(responseObj.total_days + 1);
     }
-  
-    const handlePreviousDay = () => {
-      if(currentDay > responseObj.total_days){
-        setCurrentDay(responseObj.total_days)
-      } else if (currentDay > 0) {
-        setCurrentDay(currentDay - 1)
-      }
+  };
+
+  const handlePreviousDay = () => {
+
+    if (currentDay > responseObj.total_days) {
+      setCurrentDay(responseObj.total_days);
+    } else if (currentDay > 0) {
+      setCurrentDay(currentDay - 1);
     }
-
-    const handleItemPress = (attraction) => {
-      setMainActivity(attraction)
-      const activityObj = {
-        name: attraction.name,
-        location: attraction.geometry.location,
-        photos: attraction.photos? attraction.photos[0].photo_reference : null,
-        rating: attraction.rating,
-        types: attraction.types,
-        user_ratings_total: attraction.user_ratings_total
-      }
-      const newRsponseObj = {...responseObj}
-      newRsponseObj.itinerary_info[currentDay-1].main_activity = activityObj
-      setResponseObj(newRsponseObj)
-    }
-
-    const handleMoreActivities = () => {
-      return (
-        <MoreActivitiesSelection responseObj={responseObj}
-        setResponseObj={setResponseObj}/>
-      )
-    }
-  
-    return (
-      <View style={styles.container}>
-        {currentDay <= responseObj.total_days ? <Text>Chosen Main Activity For Each Day:</Text> : <Text>Let's select more activities for each day</Text>}
-        
-
-        {currentDay > 1 ? <Button title={currentDay>responseObj.total_days ? "Back to Main Activities": "Previous Day"} onPress={handlePreviousDay} /> : null}
-
-        {currentDay <= responseObj.total_days ? <Button title={ currentDay===responseObj.total_days ? 'Next Step' : `Go to day ${currentDay+1}`} onPress={handleNextDay} /> : null}
-
-        {currentDay > responseObj.total_days ? <View><MoreActivitiesSelection responseObj={responseObj}
-        setResponseObj={setResponseObj}/></View> : 
-        <View>
-        <Text>Day {currentDay}</Text>
-        <Text>Chosen Activity:</Text>
-        { Object.keys(mainActivity).length === 0 ? <Text>Select from below</Text> : <Text>{mainActivity.name}</Text> }
-        <Text>List of tourst attractions in {responseObj.location}</Text>
-        <View>
-        <FlatList 
-        data = {attractions}
-        renderItem = {(attraction) => {
-          if(attraction.item.photos!== undefined) 
-          return (
-          <TouchableOpacity onPress={() => handleItemPress(attraction.item)}>
-          <Text style={styles.itemText}>{attraction.item.name}</Text>
-          {/* { attraction.item.photos!== undefined ? <Image source = {{ uri: fetchPlacePicture(attraction.item.photos[0].photo_reference,400)}} /> : null} */}
-        </TouchableOpacity>
-          )
-        }}
-        style={styles.list}
-        />
-        </View>
-        </View>}
-      </View>
-    )
+  };
+  console.log(responseObj, 'resp obj in resp day')
+  const handleNextStep = () => {
+    console.log(responseObj, 'resp obj in handle nexy')
+    navigation.navigate('MoreActivities', {responseObj : responseObj, setResponseObj: setResponseObj,route:route, navigation:navigation})
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    footer: {
-        flexDirection: 'row',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        backgroundColor: 'white',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 6,
-        },
-        shadowOpacity: 0.37,
-        shadowRadius: 7.49,
-        elevation: 12,
-        zIndex: 5,
-        borderTopWidth: 1,
-        borderTopColor: '#14141410'
-    },
-    icon: {
-        marginLeft: 20,
-        marginRight: 20
-    },
-    scrollContainer: {
-      flexGrow: 1,
-      top: 40,
-    },
-    attractionCard: {
-      height: 300,
-      borderColor: 'black',
-      borderWidth: 1,
-      borderRadius: 20,
-      margin: 10,
-    },
-    list: {
-      maxHeight: 500,
-    },
-    itemText: {
-      padding: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#ddd',
-    }
-  })
+  const handleItemPress = (attraction) => {
+
+    setMainActivity(attraction);
+    const activityObj = {
+      name: attraction.name,
+      location: attraction.geometry.location,
+      photos: attraction.photos ? attraction.photos[0].photo_reference : null,
+      rating: attraction.rating,
+      types: attraction.types,
+      user_ratings_total: attraction.user_ratings_total,
+    };
+    const newRsponseObj = { ...responseObj };
+    newRsponseObj.itinerary_info[currentDay - 1].main_activity = activityObj;
+    setResponseObj(newRsponseObj);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.buttonsContainer}>
+        {currentDay > 1 ? (
+          <TouchableOpacity style={styles.button} onPress={handlePreviousDay}>
+            <Text style={styles.buttonText}>
+              {currentDay > responseObj.total_days
+                ? "Back to Main Activities"
+                : "Previous Day"}
+            </Text>
+          </TouchableOpacity>
+        ) : (
+          <View style={styles.placeholder} />
+        )}
+
+        {currentDay <= responseObj.total_days ? (
+          <TouchableOpacity style={styles.button} onPress={()=>{ currentDay === responseObj.total_days ? handleNextStep() : handleNextDay()}}>
+            <Text style={styles.buttonText} >
+              {currentDay === responseObj.total_days
+                ? "Next Step"
+                : `Go to day ${currentDay + 1}`}
+            </Text>
+          </TouchableOpacity>
+        ) : (
+          <View style={styles.placeholder} />
+        )}
+      </View>
+
+      <View style={styles.body}>
+        
+        {currentDay <= responseObj.total_days ? (
+          <View style={styles.textContainer}>
+            <Text style={styles.sectionTitle}>
+              Choose a Main Touristic Attraction For Day {currentDay}
+            </Text>
+          </View>
+        ) : (
+          <Text style={styles.sectionTitle}>Let's select more activities for each day</Text>
+        )}
+
+        
+          <View>
+            <View style={styles.sectionSubTitle}>
+              {Object.keys(mainActivity).length > 0 ? (
+                <Text style={styles.subTitleText}>Selected Activity: {
+                  responseObj.itinerary_info[currentDay - 1].main_activity.name ? responseObj.itinerary_info[currentDay - 1].main_activity.name : mainActivity.name}</Text>
+                
+              ) : ( <Text style={{textAlign: "center"}}>Select One Location From Below</Text>
+                
+              )}
+            </View>
+
+            <View>
+              <FlatList
+                data={attractions}
+                renderItem={(attraction) => {
+                  if (attraction.item.photos !== undefined)
+                    return (
+                      <TouchableOpacity
+                        onPress={() => handleItemPress(attraction.item)}
+                      >
+                        <Text style={styles.itemText}>
+                          {attraction.item.name}
+                        </Text>
+                        {/* { attraction.item.photos!== undefined ? <Image source = {{ uri: fetchPlacePicture(attraction.item.photos[0].photo_reference,400)}} /> : null} */}
+                      </TouchableOpacity>
+                    );
+                }}
+                style={styles.list}
+              />
+            </View>
+          </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    top: 0,
+    width: "100%",
+    padding: 10,
+    zIndex: 10, 
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "darkgreen",
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+  },
+  placeholder: {
+    width: 100, 
+  },
+  body: {
+    flex: 1,
+    width: "100%",
+    paddingTop: 60, 
+    paddingHorizontal: 20,
+    backgroundColor: "#14141410",
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+    textAlign: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+  },
+  sectionSubTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+    textAlign: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+  },
+  subTitleText: {
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  textContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20, 
+  },
+  list: {
+    maxHeight: 400,
+    marginBottom: 20,
+  },
+  itemText: {
+    padding: 10,
+    textAlign: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+})
+
